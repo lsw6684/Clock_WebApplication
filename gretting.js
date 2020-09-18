a4013ba1c7bafa8eaa2f251ec03b9b06b6bf2a92
lsw6684 <lsw6684@hanmail.net>
2020-09-18 23:48:51 +0900

    I got a method to handle the local storage

--- gretting.js
+++ gretting.js
@@ -5,15 +5,37 @@ const form = document.querySelector(".js-form"),
 const USER_LS = "currentUser",
   SHOWING_CN = "showing";
 
+function saveName(text){
+  localStorage.setItem(USER_LS, text);
+}
+
+function handleSubmit(event){
+  event.preventDefault();
+  const currentValue = input.value;
+  paintGreeting(currentValue);
+  saveName(currentValue);
+
+}
+
+function askForName(){
+  form.classList.add(SHOWING_CN);
+  form.addEventListener("submit", handleSubmit);
+}
+
+
+
+
+
 function paintGreeting(text){
     form.classList.remove(SHOWING_CN);
     greeting.classList.add(SHOWING_CN);
-    greeting.innerText = `Hello ${text}`
+    greeting.innerText = `Hello ${text}`;
 }
 
 function loadName(){
   const currentUser = localStorage.getItem(USER_LS);
   if(currentUser===null){      // nobody there
+    askForName();
 
   } else{                      // sombody there
     paintGreeting(currentUser);
@@ -23,7 +45,7 @@ function loadName(){
 
 
 function init(){
+  loadName();
 
 }
 init();
-localS
