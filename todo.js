80825fbecefbdbc72ea331999e636e8810c32c22
lsw6684 <lsw6684@hanmail.net>
2020-09-20 00:16:19 +0900

    I killed them allllllllll

--- todo.js
+++ todo.js
@@ -3,7 +3,21 @@ const toDoForm = document.querySelector(".js-toDoForm"),
   toDoList = document.querySelector(".js-toDoList");
 
 const TODO_LS = 'toDos';
-const toDos=[];
+
+let toDos=[];
+function deleteToDo(event){
+  const btn = event.target;
+  const li = btn.parentNode;
+  toDoList.removeChild(li);
+  const cleanToDos = toDos.filter(function(toDo){
+
+    return toDo.id !==parseInt(li.id);
+  });
+  toDos=cleanToDos;
+  saveToDos();
+
+}
+
 function saveToDos(){
   localStorage.setItem(TODO_LS, JSON.stringify(toDos));
 }
@@ -15,6 +29,7 @@ function paintToDo(text){
   const span = document.createElement("span");
   const newId = toDos.length + 1
   delBtn.innerHTML = "&#x274c";
+  delBtn.addEventListener("click", deleteToDo);
   span.innerText = text;
   li.appendChild(delBtn);
   li.appendChild(span);
